dc96567be54d2c32d15fe06bddf11b7cea7c1117
Artem <artem_270787@rambler.ru>
2017-02-12 23:21:08 +0300

    Update api.js

--- src/core/api.js
+++ src/core/api.js
@@ -60,50 +60,16 @@ const api = {
 			})
 	},
 
-	// deleteSelectedItemFromDB: (itemId) => {
-	// 	return axios.post('/api/items/' + itemId);
-	// },
-
-	// moveSelectedItemFromDB: (itemId, listId) => {
-	// 	axios.put(`/api/items/${  itemId}`)
-	// },
-	//
-	// addUserIntoDB: (user) => {
-	// 		return axios.get('/api/users/uid').then((response) => {
-	// 			let newId = 1, maxId = 0;
-	// 			console.log('get last uid');
-	// 			console.log(response);
-	// 			if (response.data.length > 0){
-	// 				maxId = response.data[0].id;
-	// 				newId = ++maxId;
-	// 			} else {
-	// 				newId = 1;
-	// 			}
-	// 			return newId;
-	// 		}).then((newId)=>{
-	// 			axios.post('/api/users', {
-	// 	      id: newId,
-	// 				fio: user.fio,
-	// 				birthDate: user.birthDate,
-	// 				address: user.contacts.address,
-	// 				city: user.contacts.city,
-	// 				phone: user.contacts.phone
-	// 	    });
-	// 		});
-	// 	},
-	//
-	// editSelectedUserFromDB: (id, user) => {
-	//  return axios.put('/api/users', {
-	// 		params: {
-	// 			id: id,
-	// 			userData: user
-	// 		}
-	// 	});
-	// },
-	//
-	// deleteUserFromDB: (uid) => {
-	// 	return axios.post('/api/users/'+uid);
-	// 	}
+	changeListIteminDB: (itemId, newInfo) => {
+		return axios({
+				method: 'put',
+				url: `/api/items`,
+				params: {
+					item: itemId,
+					newInfo: newInfo
+				}
+			})
+	},
 };
 
 export default api;
